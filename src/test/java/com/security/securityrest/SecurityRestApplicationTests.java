d42c0ba160ca2329dc5aeb4e17d8a7f02626f365
grigorii <grigoriiborodin@yandex.ru>
2022-06-18 00:15:48 +0300

    initial commit

---
+++ src/test/java/com/security/securityrest/SecurityRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.securityrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
